fbf13ba8f9a5f3ebb69d447a5a295415f9a06c72
sergiorbf <sbittencourt19@gmai.com>
2024-05-03 23:45:09 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Header} from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <div>
+        <Header>
+          
+        </Header>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
