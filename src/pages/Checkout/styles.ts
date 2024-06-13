75c8824a2d6f76c64786d7f3d16ae02427481756
sergiorbf <sbittencourt19@gmail.com>
2024-06-12 21:05:43 -0300

    add some media query on checkout component

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -3,27 +3,42 @@ import { TextPatterns } from '../../styles/TextPatterns'
 
 export const Container = styled.main`
   display: flex;
+  flex-direction: column;
   margin: 0 auto;
   max-width: 72.5rem;
-  align-items: flex-end;
+  align-items: center;
   justify-content: space-between;
   gap: 2.5rem;
   padding: 5rem 1.25rem;
 
   img {
     margin-bottom: -13px;
+    width: 100%;
+    max-width: 30rem;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: flex-end;
   }
 `
 export const Order = styled.section`
   display: flex;
   flex-direction: column;
   gap: 2.5rem;
+  width: 100%;
+  max-width: 40rem;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 `
 
 export const Heading = styled.div`
   display: flex;
   gap: 4px;
   flex-direction: column;
+  text-align: center;
 
   h2 {
     ${TextPatterns.fonts.titleL}
@@ -33,6 +48,10 @@ export const Heading = styled.div`
   span {
     ${TextPatterns.fonts.textL}
     color: ${({ theme }) => theme.colors['base-subtitle']}
+  }
+
+  @media (min-width: 768px) {
+    text-align: left;
   }
 `
 export const Info = styled.div`
@@ -65,6 +84,14 @@ export const InfoContent = styled.div`
     > div div {
       display: flex;
       flex-direction: column;
+    }
+  }
+
+  @media (min-width: 768px) {
+    justify-content: space-between;
+
+    > div {
+      flex-direction: row;
     }
   }
 `
